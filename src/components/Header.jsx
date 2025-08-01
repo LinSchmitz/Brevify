b6b1832d63ef0fdcba9bf93a4b5ea909c31866e8
naleilan <leilanschmitz@gmail.com>
2025-08-01 11:42:50 +0330

    Added favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
   return (
     <header>
-      <img src="./img/logo.jpg" alt="logo" />
+      <img src="./img/logo.png" alt="logo" />
       <h1>Intelisum</h1>
     </header>
   );
